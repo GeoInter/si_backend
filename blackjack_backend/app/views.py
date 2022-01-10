1f1e526b8fb910261b6bddf4a2c85181cacb6f3a
GeoInter <phantom.link777@gmail.com>
2022-01-10 17:00:27 +0100

    add validation of incoming json

--- blackjack_backend/app/views.py
+++ blackjack_backend/app/views.py
@@ -1,10 +1,12 @@
 from http import HTTPStatus
+from django.http import response
 from django.http.response import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 
 import json
+import re 
 
 from app.models import Strategy
 
@@ -12,36 +14,38 @@ from app.models import Strategy
 @api_view(['POST'])
 def get_action(request, format=json):
     payload = json.loads(request.body)
-        
+
+    if(not is_JSON_valid(payload)):
+        return sendResponse(HTTPStatus.BAD_REQUEST, {'Error': 'Errors in transmitted JSON'})
+
     p_cards = payload['player_cards'] # array
     d_card = payload['dealer_card'] # single element 2-10 or A
-    nrDecks = payload['nrDecks'] # 1,2 or 4
-    isSoft = payload['isSoft'] # boolean
-    isDAS = payload['isDAS'] # boolean
+    nrDecks = payload['nrDecks']
+    isSoft = payload['isSoft']
+    isDAS = payload['isDAS']
 
     player_hand = ""
-    if("A" in p_cards):
-        # contains an ace
-        print("found an ace")
-        if p_cards.index("A") == 0:
-            player_hand = "A-" + p_cards[1]
-        else:
-            player_hand = "A-" + p_cards[0]
-
-    elif len(p_cards) != len(set(p_cards)):
+    if len(p_cards) != len(set(p_cards)) and len(p_cards) == 2:
         # contains at least one pair of the same value
         print("found a pair")
         player_hand = p_cards[0] + "-" + p_cards[1]
-        
     else:
         sum = 0
-        for card in p_cards:
-            sum += int(card)
-        if sum <= 7:
+        if ("A" in p_cards):
+            aceIndex = p_cards.index("A")
+        else:
+            aceIndex = -1
+        for i in range(len(p_cards)):
+            if i != aceIndex: # skip ace for addition of values
+                sum += int(p_cards[i])
+
+        if ("A" in p_cards):
+            player_hand = "A-" + str(sum)
+        elif sum <= 7: 
             player_hand = "5-7"
         else:
             player_hand = str(sum)
-            print("sum = " + player_hand)
+        print("no pair - player hand is: " + player_hand)
     
 
     isSoftInt = int(isSoft)
@@ -50,20 +54,13 @@ def get_action(request, format=json):
 
     try:
         strategy = Strategy.objects.get(player_cards=player_hand, dealer_cards=d_card, rulesetId=rulesetId)
-        message = { 'action': strategy.action}
-        httpcode = HTTPStatus.OK
+        return sendResponse(HTTPStatus.OK, { 'action': strategy.action})
     except Strategy.DoesNotExist:
-        message = { 'Error': 'No Strategy for this scenario found'}
-        httpcode = HTTPStatus.INTERNAL_SERVER_ERROR
+        return sendResponse(HTTPStatus.INTERNAL_SERVER_ERROR, { 'Error': 'No Strategy for this scenario found'})
     except Strategy.MultipleObjectsReturned:
-        message = { 'Error': 'Found multiple Strategies instead of one - database redundancy'}
-        httpcode = HTTPStatus.INTERNAL_SERVER_ERROR
+        return sendResponse(HTTPStatus.INTERNAL_SERVER_ERROR, { 'Error': 'Found multiple Strategies instead of one - database redundancy'})
     except:
-        message = { 'Error': 'Internal Server Error'}
-        httpcode = HTTPStatus.INTERNAL_SERVER_ERROR
-    response = JsonResponse(message)
-    response.status_code = httpcode
-    return response
+        return sendResponse(HTTPStatus.INTERNAL_SERVER_ERROR, { 'Error': 'Internal Server Error'})
 
 
 
@@ -95,6 +92,25 @@ def set_strat(request, format=json):
     except:
         message = { 'Error': 'Internal Server Error'}
         httpcode = HTTPStatus.INTERNAL_SERVER_ERROR
+    response = JsonResponse(message)
+    response.status_code = httpcode
+    return response
+
+
+
+def is_JSON_valid(payload):
+    nrDeck = int(payload['nrDecks'])
+    if(nrDeck == 3 or nrDeck < 1 or nrDeck > 4):
+        return False
+        
+    dealer_hand = payload['dealer_card']
+    pattern = re.compile('^([2-9]|10|A)$')
+    if(not pattern.match(dealer_hand)):
+        return False
+        
+    return True
+
+def sendResponse(httpcode, message):
     response = JsonResponse(message)
     response.status_code = httpcode
     return response
